cad97c2c25b1ca27e3a1157f58411925f05aea4d
Nimesh <nimesh17@Nimeshs-Macbook-Pro.local>
2017-10-26 02:24:36 +0530

    change views to video.statistics.viewCount

--- src/index.js
+++ src/index.js
@@ -39,10 +39,10 @@ const main = async () => {
 };
 
 function prettyPrintVideo(video, index) {
-  var views = abbreviate(video.statistics.viewCount, 1);
-  var likes = abbreviate(video.statistics.likeCount, 1);
-  if (views === NaN) views = 'disabled';
-  if (likes === NaN) likes = 'disabled';
+  let views = abbreviate(video.statistics.viewCount, 1);
+  let likes = abbreviate(video.statistics.likeCount, 1);
+  if (video.statistics.viewCount === NaN) views = 'disabled';
+  if (video.statistics.viewCount === NaN) likes = 'disabled';
   console.log(`${index + 1}. ${video.snippet.title} [👀  ${views} / 👍  ${likes}]`);
   console.log(`\thttps://www.youtube.com/watch?v=${video.id}`);
   console.log();

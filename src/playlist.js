57aecc149e0b7033d6ec82c311ade3d3ede3787f
ArcticZeroo <spencer@artix.cc>
2017-10-29 11:41:36 -0400

    Allow custom comparator to be used when sorting

--- src/playlist.js
+++ src/playlist.js
@@ -4,13 +4,41 @@ const url = require('url');
 const api = require('./api');
 
 /**
- * Sorts descending based on viewCount / popularity
+ * Sorts descending based on a given property
  */
-function comparator(a, b) {
-  return b.statistics.viewCount - a.statistics.viewCount;
+function statisticsComparator(prop, a, b) {
+    let aProp = a.statistics[prop];
+    let bProp = b.statistics[prop];
+
+    if (isNaN(aProp)) {
+      aProp = 0;
+    }
+
+    if (isNaN(bProp)) {
+      bProp = 0;
+    }
+
+    return bProp - aProp;
 }
 
-async function getSortedPlaylist(playListId) {
+/**
+ * Sorts descending based on viewCount / popularity
+ */
+function viewComparator(a, b) {
+  return statisticsComparator('viewCount', a, b);
+}
+
+function likeComparator(a, b) {
+  return statisticsComparator('likeCount', a, b);
+}
+
+/**
+ * Returns a sorted playlist based on a given comparator.
+ * @param playListId
+ * @param {function} comparator - the comparator to sort by.
+ * @returns {Promise.<Array.<T>|*>}
+ */
+async function getSortedPlaylist(playListId, comparator = viewComparator) {
   const videoIds = await api.getVideoIdsFromPlayList(playListId);
   const videos = await Promise.all(videoIds.map(api.getVideoDetails));
   return videos.sort(comparator);
@@ -22,4 +50,4 @@ function getPlaylistId(playlistUrl) {
   return parsedUrl.query.list;
 }
 
-module.exports = { getSortedPlaylist, getPlaylistId };
+module.exports = { getSortedPlaylist, getPlaylistId, viewComparator, likeComparator };
